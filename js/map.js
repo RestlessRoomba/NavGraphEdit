752cf5b93e1ec4cffb5e1c1833d9ed37267489a9
Simon Bretschneider <simonxbretschneider@gmail.com>
2026-05-11 10:10:57 +0200

    Satellite Map aktuelle Version 11.05

--- js/map.js
+++ js/map.js
@@ -1,5 +1,21 @@
-export const map = L.map('map').setView([50.98720771879944, 12.973063896924302], 16)   // Init map
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{  // Tile layer
+const osmLayer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{  // Tile layer
     maxZoom: 19 // 19 is max supported by OpenStreetMap
-}).addTo(map);
+})
+
+const satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+});
+
+export const map = L.map('map', {
+    center: [50.98720771879944, 12.973063896924302],
+    zoom: 19,
+    layers: [osmLayer]
+})  // Init map
+
+// names of layers in swith-button
+const baseMaps = {
+    "Map": osmLayer,
+    "Satellite": satelliteLayer
+};
+
+// add switch-button
+L.control.layers(baseMaps).addTo(map);
